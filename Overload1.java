d24aa26d1303ecb30387bde0836d1d2bf3e374f7
23335a0504raju <160098394+23335a0504raju@users.noreply.github.com>
2024-03-12 21:43:06 +0530

    Add files via upload

---
+++ Overload1.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+class Client{
+    void displaymsg(){
+        System.out.println("Client work is done by the manager");
+    }
+    void displaymsg(String name){
+        System.out.println("Manager is in herted from client class "+name);
+    }
+}
+public class Overload1{
+public static void main(String args[]){
+   Client obj=new Client();
+    obj.displaymsg();
+    System.out.println("Enter name if your are the manager:");
+    Scanner input=new Scanner(System.in);
+    String name;
+    name=input.next();
+    obj.displaymsg(name);
+}
+}
